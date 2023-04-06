d0c16409bbe91eb18dfbf5870d13c9b89399596d
kamil116rus <kamil.hakimov78@gmail.com>
2023-04-07 00:32:21 +0300

    nna_segodna_VSE

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -13,19 +13,20 @@
 #define MIN_SPEED 300000
 #define STEP_SPEED 10000
 
+int kbhit();
 void intro();
 void text();
 void game(char **buffer);
 char **born_buffer();
-void free_buffer(char *buffer);
+void free_buffer(char **buffer);
 int load_buffer(FILE *file, char **buffer);
 void draw(char **buffer, int gen, int pop);
 void life(char **buffer, char **buffer_old);
-void control(char *c);
-int count_life(int a, int b, buffer);
-int norm_i(int a, int i);
-int norm_j(int b, int j);
-void population(int *pop);
+void control(char *c, int *speed);
+int count_life(int a, int b, char **buffer);
+int norm_i(int i);
+int norm_j(int j);
+void population(int *pop, char **buffer);
 
 int main() {
     intro();
@@ -36,13 +37,14 @@ void intro() {
     FILE *file;
     char c;
     int memory_no = 0;
-    char **buffer = load_buffer();
+    char **buffer = born_buffer();
     if (buffer != NULL) memory_no = 1;
     while (c != 'q' && memory_no == 1) {
         text();
         c = getchar();
         if (c == '1') {
-            if (file = fopen("1.txt", "r") != NULL) {
+            file == fopen("1.txt", "r");
+            if (file != NULL) {
                 if (load_buffer(file, buffer)) {
                     game(buffer);
                 } else
@@ -64,8 +66,8 @@ char **born_buffer() {
 }
 
 // Функция освобождения памяти
-void free_buffer(char *buffer) {
-    for (int i = 0; i < SIZE_Y, i++) {
+void free_buffer(char **buffer) {
+    for (int i = 0; i < SIZE_Y; i++) {
         free(buffer[i]);
     }
     free(buffer);
@@ -93,18 +95,19 @@ int load_buffer(FILE *file, char **buffer) {
 
 // game function
 void game(char *buffer) {
-    int memory, int gen = 0, pop;
+    int memory, int gen = 0, pop, speed = 150000;
     char c = 'y';
     char *buffer_old = born_buffer();
     buffer_old != NULL ? memory = 1 : memory = 0;
     while (c != 'q' && memory == 1) {
         printf("\033[0d\033[2J");
-        population(*pop);
+        population(*pop, buffer);
         draw(buffer, gen, pop);  //печать
         life(buffer, buffer_old);
-        control(&c);
+        control(&c, &speed);
 
         usleep(speed);
+        gen++;
     }
     if (memory == 1) free_buffer(buffer_old);
 }
@@ -142,7 +145,8 @@ void life(char **buffer, char **buffer_old) {
     }
 }
 
-int count_life(int a, int b, buffer) {
+// Функция посчета соседних клеток
+int count_life(int a, int b, char **buffer) {
     int count = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
@@ -165,24 +169,38 @@ int norm_i(int i) {
 int norm_j(int j) {
     if (j < 0)
         j += SIZE_X;
-    else if (i >= SIZE_X)
+    else if (j >= SIZE_X)
         j = j % SIZE_X;
     return j;
 }
 
+// Функция управления скоростью и выхода
 void control(char *c, int *speed) {
     system("stty -icanon crtkill");
     while (kbhit() != 0) {
         *c = tolower(getchar());
-        if (c == 'a') *speed -= STEP_SPEED;
-        if (c == 'z') *speed += STEP_SPEED;
+        if (*c == 'a') *speed -= STEP_SPEED;
+        if (*c == 'z') *speed += STEP_SPEED;
     }
 }
 
-void population(int *pop) {
+// Функция подсчета живых клеток
+void population(int *pop, char **buffer) {
     for (int i = 0; i < SIZE_Y; i++) {
         for (int j = 0; j < SIZE_X; j++) {
             if (buffer[i][j] == 'o') *pop += 1;
         }
     }
+}
+
+int kbhit() {  // Функция kbhit() возвращает истину, если нажата какая-либо
+               // клавиша на клавиатуре.
+    struct timeval tv;  // В против­ном случае возвращается 0.
+    fd_set fds;  //  В любом случае код клавиши не удаляется из входного буфера.
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
 }
