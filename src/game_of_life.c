ca9f9c76f22885926b29b08813c13a9676edebf5
kamil116rus <kamil.hakimov78@gmail.com>
2023-04-06 21:50:12 +0300

    game

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,19 +1,20 @@
 /* GAME OF LIFE*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define SIZE_X 80
 #define SIZE_Y 25
 #define MAX_SPEED 20000
 #define MIN_SPEED 300000
 #define STEP_SPEED 10000
 
-#include <stdio.h>
-
 void game();
 void intro();
-void life(char *buffer);
-char *born_buffer();
+void life(char **buffer);
+char **born_buffer();
 void free_buffer(char *buffer);
-int load_buffer(FILE *file, char *buffer);
+int load_buffer(FILE *file, char **buffer);
 
 int main() {
     game();
@@ -23,22 +24,59 @@ int main() {
 void game() {
     FILE *file;
     char c;
-    char *buffer = load_buffer();
-    if (buffer != NULL) {
-        while (c != 'q') {
-            intro();
-            c = getchar();
-            if (c == '1') {
-                if (file = fopen("1.txt", "r") != NULL) {
-                    if (load_buffer(file, buffer)) {
-                        life(buffer);
-                    } else
-                        printf("file is empty or invalid\n");
+    int memory_no = 0;
+    char **buffer = load_buffer();
+    if (buffer != NULL) memory_no = 1;
+    while (c != 'q' && memory_no == 1) {
+        intro();
+        c = getchar();
+        if (c == '1') {
+            if (file = fopen("1.txt", "r") != NULL) {
+                if (load_buffer(file, buffer)) {
+                    life(buffer);
                 } else
-                    printf("no file\n");
+                    printf("file is empty or invalid\n");
+            } else
+                printf("no file\n");
+        }
+    }
+    free_buffer(buffer);
+}
+
+//Функция создания матрицы
+char **born_buffer() {
+    char **buffer = calloc(SIZE_Y, sizeof(char *));
+    for (int i = 0; i < SIZE_Y; i++) {
+        buffer[i] = calloc(SIZE_X, sizeof(char));
+    }
+    return buffer;
+}
+
+// Функция освобождения памяти
+void free_buffer(char *buffer) {
+    for (int i = 0; i < SIZE_Y, i++) {
+        free(buffer[i]);
+    }
+    free(buffer);
+}
+
+//Функция заполнения матрицы из файла
+int load_buffer(FILE *file, char **buffer) {
+    int sum = 0;
+    for (int i = 0; i < SIZE_Y; i++) {
+        for (int j = 0; j < SIZE_X; j++) {
+            char c = getchar();
+            if (c == '\n') break;
+            if (c == 'O') {
+                buffer[i][j] = ' ';
+                sum++;
+            } else if (c == ' ') {
+                buffer[i][j] = 'o';
+                sum++;
             }
         }
-        free_buffer(buffer);
-    } else
-        printf("no memory\n");
+    }
+    return sum == 0 ? 1 : 0;
 }
+
+void life(char *buffer);
--- src/go.sh
+++ src/go.sh
@@ -2,12 +2,14 @@
 
 parametr1=$1
 
+parametr2=$2
+
 script_name=$0 #присваиваем переменной script_name значение имени скрипта
 
-echo "Вы запустили скрипт с именем $script_name и параметром $parametr1"
+echo "Вы запустили скрипт с именем $script_name и параметром $parametr1 и $parametr2"
 
 clang-format -i game_of_life.c
 
 git add . 
-git commit -m"$parametr1"
+git commit -m"$parametr1 $parametr2"
 git push origin main
