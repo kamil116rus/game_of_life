6e1fd7248c445baa465d1d2fdb168702f8b3d768
kamil116rus <kamil.hakimov78@gmail.com>
2023-04-06 22:18:59 +0300

    game переименовал_несколько_функций

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>  // для паузы
 
 #define SIZE_X 80
 #define SIZE_Y 25
@@ -9,31 +10,34 @@
 #define MIN_SPEED 300000
 #define STEP_SPEED 10000
 
-void game();
 void intro();
-void life(char **buffer);
+void text();
+void game(char **buffer);
 char **born_buffer();
 void free_buffer(char *buffer);
 int load_buffer(FILE *file, char **buffer);
+void draw(char **buffer);
+void life(char **buffer, char **buffer_old);
+void control(char *c);
 
 int main() {
-    game();
+    intro();
     return 0;
 }
 
-void game() {
+void intro() {
     FILE *file;
     char c;
     int memory_no = 0;
     char **buffer = load_buffer();
     if (buffer != NULL) memory_no = 1;
     while (c != 'q' && memory_no == 1) {
-        intro();
+        text();
         c = getchar();
         if (c == '1') {
             if (file = fopen("1.txt", "r") != NULL) {
                 if (load_buffer(file, buffer)) {
-                    life(buffer);
+                    game(buffer);
                 } else
                     printf("file is empty or invalid\n");
             } else
@@ -80,4 +84,22 @@ int load_buffer(FILE *file, char **buffer) {
     return sum == SIZE_X * SIZE_Y ? 1 : 0;
 }
 
-void life(char *buffer) {}
+// game function
+void game(char *buffer) {
+    int memory;
+    char c = 'y';
+    char *buffer_old = born_buffer();
+    buffer_old != NULL ? memory = 1 : memory = 0;
+    while (c != 'q' && memory == 1) {
+        printf("\033[0d\033[2J");
+        draw(buffer);  //печать
+        life(buffer, buffer_old);
+        control(&c);
+
+        usleep(speed);
+    }
+}
+
+void draw(char **buffer);
+void life(char **buffer, char **buffer_old);
+void control(char *c);
